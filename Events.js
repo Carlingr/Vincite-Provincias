e66fe121ab242fcdc6cf22ce40c45827770c593f
Carlingr <kittyclubco@gmail.com>
2016-12-06 11:36:43 -0500

    Move Events to new file

--- Events.js
+++ Events.js
@@ -1,1 +1,19 @@
+function mousePressed() { //when the mouse is pressed
+  for (var i = 0; i < countries.length; i++) { //go through the countries
+    countries[i].invade() //if a country is clicked, invade it.
+  }
+}
 
+function windowResized() {
+  var backWidth = 992;
+  var backHeight = 687;
+  scale(1 / loc.scle)
+  if (backWidth / backHeight > windowWidth / windowHeight) { //if the limiting dimention is  Width
+    loc.scle = windowWidth / backWidth; // use the width to determine the scale
+  } else { //if the limiting dimention is height
+    loc.scle = windowHeight / backHeight; // use the height to determine the scale
+  }
+
+  scale(loc.scle)
+  resizeCanvas(windowWidth, windowHeight);
+}
