5d9324d8c273ca1f75c5297ab91d7852962ba4ea
Carlingr <kittyclubco@gmail.com>
2016-12-06 11:44:36 -0500

    remove scaling
    moved scaling to draw()

--- Events.js
+++ Events.js
@@ -7,13 +7,10 @@ function mousePressed() { //when the mouse is pressed
 function windowResized() {
   var backWidth = 992;
   var backHeight = 687;
-  scale(1 / loc.scle)
   if (backWidth / backHeight > windowWidth / windowHeight) { //if the limiting dimention is  Width
     loc.scle = windowWidth / backWidth; // use the width to determine the scale
   } else { //if the limiting dimention is height
     loc.scle = windowHeight / backHeight; // use the height to determine the scale
   }
-
-  scale(loc.scle)
   resizeCanvas(windowWidth, windowHeight);
 }
