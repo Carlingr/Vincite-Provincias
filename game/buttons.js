2708169590c0a78054393156c26034d22dfd3458
Carlingr <kittyclubco@gmail.com>
2016-12-17 10:04:40 -0500

    Update buttons.js

--- game/buttons.js
+++ game/buttons.js
@@ -17,7 +17,7 @@ function newTeam() { // make a new team
     //Blue is the same color used for rivers
   if (owners.length < colors.length + 1) { //if we have NOT run out of colors
     owners.push(new Owner(nameInput.value(), colors[owners.length - 1])) // add an owner with the name input
-    createP("New team created with the name" + nameInput.value()) //make a paragraph so we know what is going on
+    createP("New team created with the name <b>" + nameInput.value() + "</b>") //make a paragraph so we know what is going on
   } else { //if we HAVE run out of colors
     createP("Error! Max Teams") // throw a fit
     strtGme() // get us outta here
