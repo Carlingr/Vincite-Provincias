165affab73ec2833b0f3071ddf4c3e84f6a70de0
Carlingr <kittyclubco@gmail.com>
2016-12-16 20:49:13 -0500

    Fix Bugs
    replaced hide with remove
    make p when team created

--- game/buttons.js
+++ game/buttons.js
@@ -5,9 +5,9 @@ function lstTeam() { // make the last team
 
 function strtGme() { // start the game
   //<get rid of the input garbage>
-  nameInput.hide()
-  StrtGmeBtn.hide()
-  AddTeamBtn.hide()
+  nameInput.remove()
+  StrtGmeBtn.remove()
+  AddTeamBtn.remove()
     //</get rid of the input garbage>
   canvas.show() // show us the board
 }
@@ -17,6 +17,7 @@ function newTeam() { // make a new team
     //Blue is the same color used for rivers
   if (owners.length < colors.length + 1) { //if we have NOT run out of colors
     owners.push(new Owner(nameInput.value(), colors[owners.length - 1])) // add an owner with the name input
+    createP("New team created with the name" + nameInput.value()) //make a paragraph so we know what is going on
   } else { //if we HAVE run out of colors
     createP("Error! Max Teams") // throw a fit
     strtGme() // get us outta here
