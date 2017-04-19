4d536deb33102b67c70ca74a773a05ae98c49636
Carlingr <kittyclubco@gmail.com>
2017-04-19 14:58:02 -0400

    Uses New map

--- game/scripts/methods.js
+++ game/scripts/methods.js
@@ -1,21 +1,12 @@
 function mousePressed() { //when the mouse is pressed
-  if (countries) {
-    for (var i = 0; i < countries.length; i++) { //go through the countries
-      if ((abs(mouseX - ((countries[i].txtX + loc.x) * loc.scle)) < textWidth(countries[i].name) / 2) && (abs(mouseY - ((countries[i].txtY + loc.y) * loc.scle)) < titleSize)) { //if a country is clicked
+  if (loaded >= 2) { //if both the JSON and the background are loaded
+    for (var i = 0; i < countries.length; i++) { //loop through the countries
+      if (collidePointPoly(mouseX, mouseY, countries[i].path)) {
         countries[i].owner++; //cycle through the owners
         if (countries[i].owner > owners.length - 1) { // restart if we have run out of owners
-          countries[i].owner = 0
+          countries[i].owner = 0;
         }
       }
     }
   }
-}
-
-function windowResized() {
-  if (backWidth / backHeight > windowWidth / windowHeight) { //if the limiting dimention is  Width
-    loc.scle = windowWidth / backWidth; // use the width to determine the scale
-  } else { //if the limiting dimention is height
-    loc.scle = windowHeight / backHeight; // use the height to determine the scale
-  }
-  resizeCanvas((backWidth * loc.scle) + loc.x, (backHeight * loc.scle) + loc.y); //make the canvas fit the background
 }
