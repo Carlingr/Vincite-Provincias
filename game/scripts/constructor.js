7c72a1d96b1780c856eb50f0accda4fb1340ca5d
carlingr <kittyclubco@gmail.com>
2017-04-30 16:18:14 -0400

    Add Wall to JSON and points to teams
    game now stores country's wall state and the points of the team

--- game/scripts/constructor.js
+++ game/scripts/constructor.js
@@ -3,7 +3,8 @@ Owner takes the name and the color
 Owners are the teams that are used
 */
 
-function Owner(name, col) {
+function Owner(name, col, points) {
   this.name = name
   this.col = col
+  this.points = points
 }
