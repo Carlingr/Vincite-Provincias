5ce0c86eccec5317a82dc278298a11209bb9c949
carlingr <kittyclubco@gmail.com>
2017-04-30 16:18:40 -0400

    Revert "Add Wall to JSON and points to teams"
    This reverts commit 7c72a1d96b1780c856eb50f0accda4fb1340ca5d.

--- game/scripts/constructor.js
+++ game/scripts/constructor.js
@@ -3,8 +3,7 @@ Owner takes the name and the color
 Owners are the teams that are used
 */
 
-function Owner(name, col, points) {
+function Owner(name, col) {
   this.name = name
   this.col = col
-  this.points = points
 }
